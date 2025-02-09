c16b46c5fa9bea78b0fbd5ee6e6360d87b2ca148
elifdev <elifdevpro@gmail.com>
2025-02-09 21:15:46 +0300

    update

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-const toogleModal = () => {
+let bookList = [];
+
+const toggleModal = () => {
     const cartModal = document.querySelector(".cart-modal"); 
     cartModal.classList.toggle("active");
 }
+
+const getBooks = () => {
+    fetch("./products.json").then((res) = res.json()).then((books) => (bookList = books));
+}
+
+getBooks();
+
+const createBookItemsHtml = () => {
+    const bookListEl = document.querySelector(".book-list");
+    let bookListHtml = "";
+    bookList.forEach(book => {
+        bookListHtml += ``;
+    });
+
+    bookListEl.innerHTML = bookListHtml;
+};
